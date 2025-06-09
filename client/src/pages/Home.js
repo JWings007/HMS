634a0c6427614e68a931daf335ef7c957f78bbc9
syed-roshan01 <61007209+syed-roshan01@users.noreply.github.com>
2025-06-09 16:21:34 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -231,25 +231,7 @@ function Home() {
       <div className="w-full">
         <Gallery />
       </div>
-          <div className="w-full mt-20 flex flex-col items-center justify-center gap-4 px-4">
-  <h2 className="text-2xl font-bold text-center text-green-700">
-    Download Our App Now!
-  </h2>
-  <a
-    href="https://play.google.com/store/apps/details?id=com.hmsegg.eggport"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <img
-      src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
-      alt="Get it on Google Play"
-      className="h-16 md:h-12 transition-transform duration-200 hover:scale-105"
-    />
-  </a>
-</div>
-
-
-
+          
       <div>
         <Footer />
       </div>
