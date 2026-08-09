0c897ad6ae60c4a87952e37ab284f7c75b4923da
JWinngs007 <varun3112003@gmail.com>
2026-08-09 08:28:19 +0530

    Update theme color and enhance meta tags in index.html; comment out unused DNS code in index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// const dns = require("dns");
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const app = express();
 const cors = require("cors");
